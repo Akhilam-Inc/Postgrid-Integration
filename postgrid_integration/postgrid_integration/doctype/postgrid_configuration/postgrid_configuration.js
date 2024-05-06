f19711362e69c5cf880919bdacfc03c203055f2d
tailorraj <tailorraj111@gmail.com>
2024-05-06 11:54:08 +0500

    Initial commit

---
+++ postgrid_integration/postgrid_integration/doctype/postgrid_configuration/postgrid_configuration.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2024, Akhilam Inc. and contributors
+// For license information, please see license.txt
+
+frappe.ui.form.on('Postgrid Configuration', {
+	// refresh: function(frm) {
+
+	// }
+});
