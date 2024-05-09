f6e32682e491dfaf647b7f36d2201fcf5623b6dd
l0gesh29 <logeshperiyasamy24@gmail.com>
2024-05-09 13:13:57 +0530

    fix: url added

--- postgrid_integration/postgrid_integration/doctype/postgrid_configuration/postgrid_configuration.py
+++ postgrid_integration/postgrid_integration/doctype/postgrid_configuration/postgrid_configuration.py
@@ -24,12 +24,12 @@ class PostgridConfiguration(Document):
 			webhook_list = send_request(frappe._dict({
 								"method" : "GET",
 								"url" : f"{self.postgrid_url}/print-mail/v1/webhooks",
-								"headers": get_webhook_headers(postgrid_api_key=self.postgrid_api_key),
+								"headers": get_webhook_headers(postgrid_api_key=self.get_password("postgrid_api_key")),
 								"webhook": True,
 								"throw_message": "We are unable to fetch webhook list",
 			}), webhook=True)
 
-			if webhook_list.get("data"):
+			if webhook_list and webhook_list.get("data"):
 				webhook_created = False
 				for row in webhook_list.get("data"):
 					if row.get("enabled") and "cheque.updated" in row.get("enabledEvents") and "postgrid_integration.api.cheque_update" in row.get("url"):
@@ -39,9 +39,9 @@ class PostgridConfiguration(Document):
 					args = frappe._dict({
 						"method" : "POST",
 						"url" : f"{self.postgrid_url}/print-mail/v1/webhooks",
-						"headers": get_headers(postgrid_api_key=self.postgrid_api_key),
+						"headers": get_headers(postgrid_api_key=self.get_password("postgrid_api_key")),
 						"webhook": True,
-						"payload": get_payload(create_webhook=True, url=frappe.request.origin),
+						"payload": get_payload(create_webhook=True, url=frappe.request.origin+"/api/method/postgrid_integration.api.cheque_update"),
 						"throw_message": "We are unable to create webhook",
 					})
 					send_request(args, webhook=True)
