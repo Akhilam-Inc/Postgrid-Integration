f673fdd066fd0fa4f883b74885aeb07ef5a8c915
l0gesh29 <logeshperiyasamy24@gmail.com>
2024-06-07 21:15:27 +0530

    feat: filter added for print format

--- postgrid_integration/postgrid_integration/doctype/postgrid_configuration/postgrid_configuration.js
+++ postgrid_integration/postgrid_integration/doctype/postgrid_configuration/postgrid_configuration.js
@@ -2,7 +2,16 @@
 // For license information, please see license.txt
 
 frappe.ui.form.on('Postgrid Configuration', {
-	// refresh: function(frm) {
-
-	// }
+	refresh: function(frm) {
+		frm.fields_dict['postgrid_letter_format'].grid.get_field('format').get_query = function(doc, cdt, cdn) {
+			var row = locals[cdt][cdn];
+			if(row.doctype_name){
+				return {
+					filters: [
+						['Print Format', 'doc_type', 'in', [row.doctype_name]]
+					]
+				};
+			}
+		};
+	}
 });
