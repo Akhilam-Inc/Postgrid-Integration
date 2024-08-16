90d979e76da8a0fadda5c97b7a5410d5cf1ab59d
l0gesh29 <logeshperiyasamy24@gmail.com>
2024-08-16 12:29:36 +0530

    refactore: validation added for postgrid enable

--- postgrid_integration/postgrid_integration/doctype/bulk_letter_creation_tool/bulk_letter_creation_tool.js
+++ postgrid_integration/postgrid_integration/doctype/bulk_letter_creation_tool/bulk_letter_creation_tool.js
@@ -7,25 +7,32 @@ frappe.ui.form.on('Bulk Letter Creation Tool', {
 			frappe.msgprint(frm.doc.invalid_invoices)
 		}
 		frm.add_custom_button("Send Letter", function(){
-			if(cur_frm.fields_dict.items.grid.get_selected_children().length == 0){
-				frappe.throw("Select any Invoice in Items Table to Send Letter")
-			}
-			if(frm.doc.items){
-				frappe.call({
-					method: "postgrid_integration.postgrid_integration.doctype.bulk_letter_creation_tool.bulk_letter_creation_tool.process_bulk_letter",
-					args: {
-							invoice_list: cur_frm.fields_dict.items.grid.get_selected_children()
-					},
-					freeze: true,
-					freeze_message: __("Processing Letters..."),
-					callback: function (r) {
+			frappe.db.get_value("Postgrid Configuration", "Postgrid Configuration", "enable").then((res) => {
+				console.log(res.message.enable)
+				if(parseInt(res.message.enable)){
+					if(cur_frm.fields_dict.items.grid.get_selected_children().length == 0){
+						frappe.throw("Select any Invoice in Items Table to Send Letter")
+					}
+					if(frm.doc.items){
+						frappe.call({
+							method: "postgrid_integration.postgrid_integration.doctype.bulk_letter_creation_tool.bulk_letter_creation_tool.process_bulk_letter",
+							args: {
+									invoice_list: cur_frm.fields_dict.items.grid.get_selected_children()
+							},
+							freeze: true,
+							freeze_message: __("Processing Letters..."),
+							callback: function (r) {
 
-					},
-				})
-			}
-			else{
-				frappe.msgprint("In Order to Send Postgrid Letter Fetch the Invoices.")
-			}
+							},
+						})
+					}
+					else
+						frappe.msgprint("In Order to Send Postgrid Letter Fetch the Invoices.")
+				}
+				else
+					frappe.throw("Postgrid Integration is not enabled. Enable it in Postgrid Configuration DocType.")
+
+			})
 		});
 	}
 });
